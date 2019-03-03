6228f5a95291b2cdc6166144475adc268400c02f
Raccoon_kek <Lyuda-mirosh@mail.ru>
2019-03-03 23:30:23 +0500

    Initial commit

---
+++ robots/src/gui/GameVisualizer.java
@@ -0,0 +1,130 @@
+package gui;
+
+import primitives.Robot;
+import primitives.Target;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.AffineTransform;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GameVisualizer extends JPanel
+{
+    private final Timer m_timer = initTimer();
+    
+    private static Timer initTimer() 
+    {
+        Timer timer = new Timer("events generator", true);
+        return timer;
+    }
+
+    private Robot robot = new Robot(100, 100);
+    private Target target = new Target(150, 100);
+
+    public GameVisualizer() 
+    {
+        m_timer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                onRedrawEvent();
+            }
+        }, 0, 50);
+        m_timer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                onModelUpdateEvent();
+            }
+        }, 0, 10);
+        addMouseListener(new MouseAdapter()
+        {
+            @Override
+            public void mouseClicked(MouseEvent e)
+            {
+                target.setTargetPosition(e.getPoint());
+                repaint();
+            }
+        });
+        setDoubleBuffered(true);
+    }
+
+
+    protected void onRedrawEvent()
+    {
+        EventQueue.invokeLater(this::repaint);
+    }
+
+    private static double distance(double x1, double y1, double x2, double y2)
+    {
+        double diffX = x1 - x2;
+        double diffY = y1 - y2;
+        return Math.sqrt(diffX * diffX + diffY * diffY);
+    }
+
+    protected void onModelUpdateEvent()
+    {
+        double distance = distance(target.getX(), target.getY(),
+            robot.getX(), robot.getY());
+        if (distance < 0.5)
+        {
+            return;
+        }
+        robot.move(10, target.getX(), target.getY());
+    }
+    
+    private static int round(double value)
+    {
+        return (int)(value + 0.5);
+    }
+    
+    @Override
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D)g; 
+        drawRobot(g2d, round(robot.getX()), round(robot.getY()), robot.getDirection());
+        drawTarget(g2d, target.getX(), target.getY());
+    }
+    
+    private static void fillOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
+    {
+        g.fillOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
+    }
+    
+    private static void drawOval(Graphics g, int centerX, int centerY, int diam1, int diam2)
+    {
+        g.drawOval(centerX - diam1 / 2, centerY - diam2 / 2, diam1, diam2);
+    }
+    
+    private void drawRobot(Graphics2D g, int x, int y, double direction)
+    {
+        int robotCenterX = round(robot.getX());
+        int robotCenterY = round(robot.getY());
+        AffineTransform t = AffineTransform.getRotateInstance(direction, robotCenterX, robotCenterY); 
+        g.setTransform(t);
+        g.setColor(Color.MAGENTA);
+        fillOval(g, robotCenterX, robotCenterY, 30, 10);
+        g.setColor(Color.BLACK);
+        drawOval(g, robotCenterX, robotCenterY, 30, 10);
+        g.setColor(Color.WHITE);
+        fillOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+        g.setColor(Color.BLACK);
+        drawOval(g, robotCenterX  + 10, robotCenterY, 5, 5);
+    }
+    
+    private void drawTarget(Graphics2D g, int x, int y)
+    {
+        AffineTransform t = AffineTransform.getRotateInstance(0, 0, 0); 
+        g.setTransform(t);
+        g.setColor(Color.GREEN);
+        fillOval(g, x, y, 5, 5);
+        g.setColor(Color.BLACK);
+        drawOval(g, x, y, 5, 5);
+    }
+}
