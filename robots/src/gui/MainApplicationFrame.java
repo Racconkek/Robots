c984220d44e6f6060ee111688c373b27bcc40cd6
Raccoon_kek <Lyuda-mirosh@mail.ru>
2019-03-25 10:56:20 +0500

    add ask for close on X

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -2,9 +2,7 @@ package gui;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 import javax.swing.*;
 
@@ -42,7 +40,10 @@ public class MainApplicationFrame extends JFrame
         addWindow(gameWindow);
 
         setJMenuBar(generateMenuBar());
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {public void windowClosing(WindowEvent we) {
+            close(); }});
+
     }
     
     protected LogWindow createLogWindow()
@@ -117,7 +118,7 @@ public class MainApplicationFrame extends JFrame
                      (event) -> {Logger.debug("Новая строка");});
             testMenu.add(addLogMessageItem);
 
-            var exit = createJMenuItem("Выход", KeyEvent.VK_E, (event) ->{ temp();
+            var exit = createJMenuItem("Выход", KeyEvent.VK_E, (event) ->{ close();
 
             });
             programMenu.add(exit);
@@ -130,7 +131,7 @@ public class MainApplicationFrame extends JFrame
         return menuBar;
     }
 
-    private void temp(){
+    private void close(){
         UIManager.put("OptionPane.yesButtonText"   , "Да"    );
         UIManager.put("OptionPane.noButtonText"    , "Нет"   );
         var result = JOptionPane.showConfirmDialog(this,
